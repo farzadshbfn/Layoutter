16896d5334eef18ca3d07cda5939554c5482a23e
FarzadShbfn <farzad.shbfn@gmail.com>
2017-03-12 12:50:17 +0330

    Initial Commit

---
+++ Layoutter/Layoutter.h
@@ -0,0 +1,19 @@
+//
+//  Layoutter.h
+//  Layoutter
+//
+//  Created by Farzad Sharbafian on 3/12/17.
+//  Copyright © 2017 FarzadShbfn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Layoutter.
+FOUNDATION_EXPORT double LayoutterVersionNumber;
+
+//! Project version string for Layoutter.
+FOUNDATION_EXPORT const unsigned char LayoutterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Layoutter/PublicHeader.h>
+
+
